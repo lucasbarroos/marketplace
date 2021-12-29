857ee948920022b0600e6835ea85415a5e0317f2
Lucas Barros <lbarros.comp@gmail.com>
2021-12-28 23:16:44 -0300

    refactor: improving the folder structure

--- src/components/Hero/styles.ts
+++ src/components/Hero/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  min-height: 60vh;
+  height: 60vh;
   background-color: #eee;
 `;
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { GetStaticProps, InferGetServerSidePropsType } from 'next';
-import Hero from './components/Hero';
+import Hero from '../components/Hero';
 
 export const getServerSideProps: GetStaticProps = async () => {
   const response = await fetch('http://localhost:3000/api/products');
